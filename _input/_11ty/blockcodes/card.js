73444638ce368512cb3a4a4eb49d3dd692ce3066
CaptainHarrie <butchlinkle@gmail.com>
2025-01-18 23:32:26 +0000

    unset padding on card-body, add padding to macro to offset this change, update macro to be more configurable

--- _input/_11ty/blockcodes/card.js
+++ _input/_11ty/blockcodes/card.js
@@ -1,6 +1,27 @@
-export const card = (content, title, classString = "", styling = "") => {
-  const header = title ? `<div class="card-header">${title}</div>` : "";
-  const body = `<div class="card-body">${content}</div>`;
+export const card = (content, parameters) => {
+  let header;
+  if (typeof parameters === "string") {
+    header = `<div class="card-header">${parameters}</div>` ?? "";
+  } else if (typeof parameters === "object" && parameters.header) {
+    header = `<div class="card-header">${parameters.header}</div>` ?? "";
+  }
+  const img = parameters.img ? `<img class="card-img-top" src="${img}">` : "";
+  const title = parameters.title
+    ? `<h5 class="card-title">${parameters.title}</h5>`
+    : "";
+  const subtitle = parameters.subtitle
+    ? `<h6 class="card-subtitle mb-2 text-muted">${parameters.subtitle}</h6>`
+    : "";
+  const body = `<div class="card-body p-3">
+  ${title}
+  ${subtitle}
+  ${content}
+  </div>`;
+  const footer = parameters.footer
+    ? `<div class="card-footer text-muted">${parameters.footer}</div>`
+    : "";
 
-  return `<div class="card ${classString}" style="${styling}">${header}${body}</div>`;
+  return `<div class="card ${parameters.classes ?? ""}" style="${
+    parameters.styling ?? ""
+  }">${img}${header ?? ""}${body}${footer}</div>`;
 };
